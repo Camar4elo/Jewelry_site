e4fb35aa02a1cb3fe42a887d0f38749d8c7289e4
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-08-16 16:20:33 +0300

    Create Decorations, Material, Category

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from shop.models import Material, Category, Decorations
 
-# Register your models here.
+
+@admin.register(Decorations)
+class DecorationsAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['material', 'category']
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ['material']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category']
