4c294982a0ca94ece6d0979abccb75eb32aebc24
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-08-19 09:46:24 +0300

    Create ImagesAdmin, update DecorationsAdmin

--- shop/admin.py
+++ shop/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
-from shop.models import Material, Category, Decorations
+from shop.models import Material, Category, Decorations, Gems, Images
+
+
+class ImagesAdmin(admin.StackedInline):
+    model = Images
 
 
 @admin.register(Decorations)
 class DecorationsAdmin(admin.ModelAdmin):
-    list_display = ['name', 'material', 'category', 'description']
-    list_filter = ['material', 'category']
+    list_display = ('name', 'category', 'display_gems', 'display_material',
+                    'price', 'description')
+    list_filter = ('material', 'category', 'price', 'gems')
+
+    inlines = [ImagesAdmin]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category']
 
 
 @admin.register(Material)
@@ -13,6 +25,12 @@ class MaterialAdmin(admin.ModelAdmin):
     list_display = ['material']
 
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['category']
+@admin.register(Gems)
+class GemsAdmin(admin.ModelAdmin):
+    list_display = ['gems']
+
+
+@admin.register(Images)
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['decorations', 'display_image']
+    list_filter = ['decorations__name', 'decorations__category']
