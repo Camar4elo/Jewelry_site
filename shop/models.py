f7755c339728686a2ce93c34dd39fbfec30d86d0
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-08-20 12:29:51 +0300

    update Images

--- shop/models.py
+++ shop/models.py
@@ -39,8 +39,30 @@ class Gems(models.Model):
         return self.gems
 
 
+class Images(models.Model):
+    image = models.ImageField(upload_to='', verbose_name='Изображение',
+                              blank=True, null=True)
+    decoration = models.ForeignKey('Decorations', on_delete=models.CASCADE,
+                                   verbose_name='Название изделия',
+                                   blank=True,
+                                   null=True)
+
+    class Meta:
+        verbose_name = "Изображение"
+        verbose_name_plural = "Изображения"
+
+    def __str__(self):
+        return self.image.url
+
+    def display_image(self):
+        return mark_safe(f'<img src="{self.image.url}"width="100" height="100"')
+
+    display_image.short_description = 'Изображение'
+
+
 class Decorations(models.Model):
-    name = models.CharField(max_length=250, verbose_name='Название изделия')
+    name = models.CharField(max_length=250, verbose_name='Название изделия',
+                            unique=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
                                  null=True, verbose_name='Категория')
     material = models.ManyToManyField(Material, verbose_name='Материал',
@@ -51,6 +73,8 @@ class Decorations(models.Model):
                                    null=True, blank=True)
     price = models.DecimalField(max_digits=100, decimal_places=2, null=True,
                                 verbose_name='Цена')
+    image = models.ManyToManyField(Images, verbose_name='Изображения',
+                                   blank=True)
 
     class Meta:
         verbose_name = "Украшение"
@@ -60,31 +84,17 @@ class Decorations(models.Model):
         return self.name
 
     def display_gems(self):
-        return "\n".join([p.gems for p in self.gems.all()])
+        return "\n, ".join([p.gems for p in self.gems.all()])
 
     display_gems.short_description = 'Драгоценные камни'
 
     def display_material(self):
-        return "\n".join([p.material for p in self.material.all()])
+        return "\n, ".join([p.material for p in self.material.all()])
 
     display_material.short_description = 'Материал'
 
-    def display_images(self):
-        return "\n".join([p.material for p in self.material.all()])
-
-
-class Images(models.Model):
-    image = models.ImageField(upload_to='', verbose_name='Изображение',
-                              blank=True, null=True)
-    decorations = models.ForeignKey(Decorations, on_delete=models.CASCADE,
-                                    verbose_name='Название изделия',
-                                    related_name='images')
-
-    class Meta:
-        verbose_name = "Изображение"
-        verbose_name_plural = "Изображения"
-
     def display_image(self):
-        return mark_safe(f'<img src="{self.image.url}"width="100" height="100"')
+        print(f'!!!!!!!!!!!!!!!!!!!!!!!!{self.image.all()}')
+        return mark_safe(f'<img src="{self.image.filter().first().image.url}"width="100" height="100"')
 
     display_image.short_description = 'Изображение'
