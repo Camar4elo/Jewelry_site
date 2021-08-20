0822651c47c22a8f00158f827f6a5d001ac91ef4
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-08-20 14:14:43 +0300

    refactor code

--- shop/admin.py
+++ shop/admin.py
@@ -1,36 +1,37 @@
 from django.contrib import admin
-from shop.models import Material, Category, Decorations, Gems, Images
+from shop.models import Material, Category, Decoration, Gem, Image
 
 
-class ImagesAdmin(admin.StackedInline):
-    model = Images
+class ImageAdmin(admin.StackedInline):
+    model = Image
 
 
-@admin.register(Decorations)
-class DecorationsAdmin(admin.ModelAdmin):
-    # fields = ('name', 'category', ('gems', 'material'),
-    #           'price', 'description')
+@admin.register(Decoration)
+class DecorationAdmin(admin.ModelAdmin):
+    fields = ['name', 'category', ('gem', 'material'),
+              'price', 'description']
     list_display = ('name', 'category', 'display_gems', 'display_material',
                     'price', 'description', 'display_image')
-    list_filter = ('material', 'category', 'price', 'gems')
-    inlines = [ImagesAdmin]
+    list_filter = ('material', 'category', 'price', 'gem')
+    inlines = [ImageAdmin]
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['category']
+    list_display = ['name']
 
 
 @admin.register(Material)
 class MaterialAdmin(admin.ModelAdmin):
-    list_display = ['material']
+    list_display = ['name']
 
 
-@admin.register(Gems)
+@admin.register(Gem)
 class GemsAdmin(admin.ModelAdmin):
-    list_display = ['gems']
+    list_display = ['name']
 
 
-@admin.register(Images)
-class ImagesAdmin(admin.ModelAdmin):
-    list_display = ['display_image']
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['decoration', 'display_image']
+    list_filter = ['decoration']
