fd51bb5a1f56de6b8d7b9c5c89e9205f24862e0b
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-08-23 19:10:27 +0300

    create base, rings, earrings, necklaces, contacts

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MainView.as_view(), name='main'),
+    path('', views.MainView.as_view(), name='base'),
+    path('rings', views.RingsView.as_view(), name='rings'),
+    path('earrings', views.EarringsView.as_view(), name='earrings'),
+    path('necklaces', views.NecklacesView.as_view(), name='necklaces'),
+    path('contacts', views.ContactsView.as_view(), name='contacts'),
 ]
