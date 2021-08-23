4700c414ca912038c7bb06f5dd6c1d2441b3ebb8
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-08-23 19:14:46 +0300

    create RingsView, EarringsView, NecklacesView, ContactsView

--- shop/views.py
+++ shop/views.py
@@ -5,3 +5,23 @@ from django.views.generic.base import View
 class MainView(View):
     def get(self, request):
         return render(request, "index.html")
+
+
+class RingsView(View):
+    def get(self, request):
+        return render(request, "rings.html")
+
+
+class EarringsView(View):
+    def get(self, request):
+        return render(request, "earrings.html")
+
+
+class NecklacesView(View):
+    def get(self, request):
+        return render(request, "necklaces.html")
+
+
+class ContactsView(View):
+    def get(self, request):
+        return render(request, "contacts.html")
