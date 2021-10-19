6b1a63c5ca0bce0e2c2ec8e386de3a57b5b84df2
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-10-19 13:12:15 +0300

    add func: create_images_list

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-from .models import Image
+from .models import Photo
 
 
 class MainView(View):
     def get(self, request):
-        images = Image.objects.all()
-        return render(request, "base.html", {"image_list": images})
+        images = Photo.objects.all()
+        images_list = create_images_list(images)
+        return render(request, "base.html", {"images_list": images_list})
+
+
+def create_images_list(images):
+    images_list = []
+    data_aos_delay = 0
+    for image in images:
+        if data_aos_delay == 600:
+            data_aos_delay = 0
+        data_aos_delay += 100
+        images_list.append({"name": image.name, "data_aos_delay":
+                           str(data_aos_delay)})
+    return images_list
 
 
 class RingsView(View):
