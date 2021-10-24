bf0dcb6ac25ab16099c00c607d804b7d8b3e74d5
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-10-24 13:39:56 +0300

    create social network links, create_category_list func, update create_images_list func

--- shop/views.py
+++ shop/views.py
@@ -1,25 +1,50 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-from .models import Photo
+from .models import Photo, MainPhoto, Category, SocialNetwork, Decoration
 
 
 class MainView(View):
     def get(self, request):
-        images = Photo.objects.all()
-        images_list = create_images_list(images)
-        return render(request, "base.html", {"images_list": images_list})
+        photo_and_greetings = MainPhoto.objects.first()
+        social_link_instagram = SocialNetwork.objects.get(name='Instagram')
+        social_link_whatsapp = SocialNetwork.objects.get(name='Whatsapp')
+        social_link_telegram = SocialNetwork.objects.get(name='Telegram')
+        social_link_vk = SocialNetwork.objects.get(name='VK')
+        category = Category.objects.all()
+        images_list = create_images_list(category)
+        category_list = create_category_list(category)
+        return render(request, "base.html",
+                      {"images_list": images_list,
+                       "category_list": category_list,
+                       "photo_and_greetings": photo_and_greetings,
+                       "social_link_instagram": social_link_instagram.link,
+                       "social_link_whatsapp": social_link_whatsapp.link,
+                       "social_link_telegram": social_link_telegram.link,
+                       "social_link_vk": social_link_vk.link})
 
 
-def create_images_list(images):
+def create_images_list(category):
     images_list = []
-    data_aos_delay = 0
-    for image in images:
-        if data_aos_delay == 600:
-            data_aos_delay = 0
-        data_aos_delay += 100
-        images_list.append({"name": image.name, "data_aos_delay":
-                           str(data_aos_delay)})
+    for category_name in category:
+        decoration = Decoration.objects.filter(category_id=category_name.
+                                               id).get()
+        images = Photo.objects.filter(decoration_id=decoration.id)
+        data_aos_delay = 0
+        for image in images:
+            if data_aos_delay == 600:
+                data_aos_delay = 0
+            data_aos_delay += 100
+            images_list.append({"category": category_name.name,
+                                "name": image.name, "data_aos_delay":
+                                str(data_aos_delay)})
     return images_list
+
+
+def create_category_list(category):
+    category_list = []
+    for category_name in category:
+        category_list.append(str(category_name))
+    return category_list
 
 
 class RingsView(View):
