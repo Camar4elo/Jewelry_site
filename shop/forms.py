b5d594656670603d9a88a1d46bbfee3101216249
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-11-19 17:28:11 +0300

    add widgets

--- shop/forms.py
+++ shop/forms.py
@@ -2,7 +2,13 @@ from django import forms
 
 
 class ContactMe(forms.Form):
-    name = forms.CharField(label='Ваше имя:', required=True)
-    phone_number = forms.IntegerField(required=True,
-                                      label='Ваш номер телефона:')
-    message = forms.CharField(required=True, label='Сообщение:')
+    name = forms.CharField(label='Ваше имя:', required=True,
+                           widget=forms.TextInput
+                           (attrs={'class': 'contact-form'}))
+    phone_number = forms.CharField(label='Ваш номер телефона:', required=True,
+                                   widget=forms.TextInput
+                                   (attrs={'class': 'contact-form'}))
+    message = forms.CharField(label='Сообщение:', required=True,
+                              max_length=1500, widget=forms.Textarea
+                              (attrs={'class': 'contact-form',
+                               'style': 'line-height: 20px'}))
