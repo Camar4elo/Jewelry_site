368849fbb3d04348834852a73d934dc89c73a6f3
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-11-20 14:22:24 +0300

    update ContactMe form - widget attrs

--- shop/forms.py
+++ shop/forms.py
@@ -2,13 +2,16 @@ from django import forms
 
 
 class ContactMe(forms.Form):
-    name = forms.CharField(label='Ваше имя:', required=True,
-                           widget=forms.TextInput
-                           (attrs={'class': 'contact-form'}))
-    phone_number = forms.CharField(label='Ваш номер телефона:', required=True,
+    name = forms.CharField(label='', required=True,
+                           max_length=40, widget=forms.TextInput
+                           (attrs={'class': 'contact-form',
+                                   'placeholder': 'Ваше имя:'}))
+    phone_number = forms.CharField(label='', required=True,
                                    widget=forms.TextInput
-                                   (attrs={'class': 'contact-form'}))
-    message = forms.CharField(label='Сообщение:', required=True,
+                                   (attrs={'class': 'contact-form',
+                                           'placeholder': 'Ваш номер телефона:'}))
+    message = forms.CharField(label='', required=True,
                               max_length=1500, widget=forms.Textarea
                               (attrs={'class': 'contact-form',
-                               'style': 'line-height: 20px'}))
+                               'style': 'line-height: 20px',
+                                      'placeholder': 'Напишите сообщение:'}))
