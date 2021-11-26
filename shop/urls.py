ef9222e39892aa2f5254eb6a65c56e51b05c1e55
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-11-26 12:54:09 +0300

    delete: paths rings, earrings, necklaces, contacts

--- shop/urls.py
+++ shop/urls.py
@@ -3,8 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.MainView.as_view(), name='base'),
-    path('rings', views.RingsView.as_view(), name='rings'),
-    path('earrings', views.EarringsView.as_view(), name='earrings'),
-    path('necklaces', views.NecklacesView.as_view(), name='necklaces'),
-    path('contacts', views.ContactsView.as_view(), name='contacts'),
 ]
