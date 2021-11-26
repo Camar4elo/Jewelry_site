e08471bb17d373a99c5ca641ef186ac51fecdca1
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-11-26 11:46:56 +0300

    refactor code

--- shop/forms.py
+++ shop/forms.py
@@ -2,16 +2,14 @@ from django import forms
 
 
 class ContactMe(forms.Form):
-    name = forms.CharField(label='', required=True,
-                           max_length=40, widget=forms.TextInput
-                           (attrs={'class': 'contact-form',
-                                   'placeholder': 'Ваше имя:'}))
-    phone_number = forms.CharField(label='', required=True,
-                                   widget=forms.TextInput
-                                   (attrs={'class': 'contact-form',
-                                           'placeholder': 'Ваш номер телефона:'}))
-    message = forms.CharField(label='', required=True,
-                              max_length=1500, widget=forms.Textarea
-                              (attrs={'class': 'contact-form',
-                               'style': 'line-height: 20px',
-                                      'placeholder': 'Напишите сообщение:'}))
+    name = forms.CharField(max_length=40, required=True,
+                           widget=forms.TextInput(attrs={'class': 'contact-form', 'placeholder': 'Ваше имя:'}),
+                           label='')
+    phone_number = forms.CharField(max_length=16, required=True,
+                                   widget=forms.TextInput(attrs={'class': 'contact-form',
+                                                                 'placeholder': 'Ваш номер телефона:'}),
+                                   label='')
+    message = forms.CharField(max_length=1500, required=True,
+                              widget=forms.Textarea(attrs={'class': 'contact-form', 'style': 'line-height: 20px',
+                                                           'placeholder': 'Напишите сообщение:'}),
+                              label='')
