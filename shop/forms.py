dc0786191f80d93598177767d81fa574c3421aa0
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-11-28 02:51:21 +0300

    update: ContactMe phone_number add widget attrs pattern, title, remove max_length

--- shop/forms.py
+++ shop/forms.py
@@ -5,9 +5,13 @@ class ContactMe(forms.Form):
     name = forms.CharField(max_length=40, required=True,
                            widget=forms.TextInput(attrs={'class': 'contact-form', 'placeholder': 'Ваше имя:'}),
                            label='')
-    phone_number = forms.CharField(max_length=16, required=True,
+    phone_number = forms.CharField(required=True,
                                    widget=forms.TextInput(attrs={'class': 'contact-form',
-                                                                 'placeholder': 'Ваш номер телефона:'}),
+                                                                 'placeholder': 'Ваш номер телефона:',
+                                                                 'pattern': '^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?'
+                                                                 '[\d\- ]{7,10}$',
+                                                                 'title': 'Мобильный номер в формате: +7(999)999-99-99'
+                                                                 }),
                                    label='')
     message = forms.CharField(max_length=1500, required=True,
                               widget=forms.Textarea(attrs={'class': 'contact-form', 'style': 'line-height: 20px',
