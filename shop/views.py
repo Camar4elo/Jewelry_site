ad94585849069a69fcf4cacd5ae549035ed47359
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-11-30 10:41:24 +0300

    delete: photo_and_greetings, create: main_photo, main_text, refactor: create_gems_list function

--- shop/views.py
+++ shop/views.py
@@ -9,7 +9,8 @@ import math
 
 class MainView(View):
     def get(self, request):
-        photo_and_greetings = MainPhoto.objects.first()
+        main_photo = MainPhoto.objects.values('photo').first()
+        main_text = MainPhoto.objects.values('content').first()
         social_links = SocialNetwork.objects.all()
         images_list = create_images_list()
         category_list = create_category_list()
@@ -22,10 +23,11 @@ class MainView(View):
         return render(request, "base.html",
                       {"images_list": images_list,
                        "category_list": category_list,
-                       "photo_and_greetings": photo_and_greetings,
+                       "main_photo": main_photo,
                        "social_links": social_links,
                        "form": form,
                        "gems_list": gems_list,
+                       "main_text": main_text,
                        "materials_text": materials_text,
                        "contacts_text": contacts_text,
                        "delivery_text": delivery_text,
@@ -75,13 +77,8 @@ def create_gems_list():
     gems = Gem.objects.all()
     lists_amount = int(math.ceil(len(gems)/end_value))
     gems_list = []
-    len_value = 0
-    for result in range(lists_amount):
-        res_list = []
-        for gem in gems[initial_value:end_value]:
-            res_list.append(gem)
-            len_value += 1
+    for i in range(lists_amount):
+        gems_list.append(gems[initial_value:end_value])
         initial_value = end_value
         end_value += 4
-        gems_list.append(res_list)
     return gems_list
