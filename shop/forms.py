1a6914fd5588986699622a62f852d550eb7b1878
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-12-01 10:45:23 +0300

    code refactoring

--- shop/forms.py
+++ shop/forms.py
@@ -3,10 +3,10 @@ from django import forms
 
 class ContactMe(forms.Form):
     name = forms.CharField(max_length=40, required=True,
-                           widget=forms.TextInput(attrs={'class': 'contact-form', 'placeholder': 'Ваше имя:'}),
+                           widget=forms.TextInput(attrs={'class': 'form-field', 'placeholder': 'Ваше имя:'}),
                            label='')
     phone_number = forms.CharField(required=True,
-                                   widget=forms.TextInput(attrs={'class': 'contact-form',
+                                   widget=forms.TextInput(attrs={'class': 'form-field',
                                                                  'placeholder': 'Ваш номер телефона:',
                                                                  'pattern': '^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?'
                                                                  '[\d\- ]{7,10}$',
@@ -14,6 +14,6 @@ class ContactMe(forms.Form):
                                                                  }),
                                    label='')
     message = forms.CharField(max_length=1500, required=True,
-                              widget=forms.Textarea(attrs={'class': 'contact-form', 'style': 'line-height: 20px',
+                              widget=forms.Textarea(attrs={'class': 'form-field', 'style': 'line-height: 20px',
                                                            'placeholder': 'Напишите сообщение:'}),
                               label='')
